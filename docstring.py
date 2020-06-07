ec83da9b0eeec7813880263d28c6abfb35f7d466
shravanjsdev <sshravan.js@gmail.com>
2020-06-07 22:09:59 +0530

    Docstrings 2

--- docstring.py
+++ docstring.py
@@ -4,6 +4,16 @@ These are also used as comments
 But, comments are ignored by the compiler, whereas docstrings are not
 Docstrings are used to mention what to do
 Whereas comments are used to mention how to do.
+NOTE: This docstring wont get printed in the file, since there is some
+code below it
 """
 
 print("Docstrings are used to describe what the functions or program is doing")
+
+"""
+Docstrings are not omitted by the compiler, when the docstring is placed
+above any code, it will not get printed in the file, whereas 
+the docstrings that present below the code will be printed in the file
+NOTE: This docstring will get printed in the file, since there is no
+code below this docstring
+"""
