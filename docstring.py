64e2dc8666d6676b5359d0c4f2efe023341d78db
shravanjsdev <sshravan.js@gmail.com>
2020-06-07 22:04:03 +0530

    Docstrings

---
+++ docstring.py
@@ -0,0 +1,9 @@
+"""
+The strings present between the triple quotes are called as docstring
+These are also used as comments
+But, comments are ignored by the compiler, whereas docstrings are not
+Docstrings are used to mention what to do
+Whereas comments are used to mention how to do.
+"""
+
+print("Docstrings are used to describe what the functions or program is doing")
