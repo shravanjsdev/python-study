c561be2dbccaf38b453b7e73b182c643aac68b98
shravanjsdev <sshravan.js@gmail.com>
2020-06-07 22:59:33 +0530

    Number datatype

---
+++ number-datatype.py
@@ -0,0 +1,22 @@
+# Number datatype
+
+# type() method is used to return the datatype of the variable
+# We can give 'n' number of arguments to print() method separated by comma(,)
+
+# Integer
+int_var = 10  # All whole numbers without decimal are integers
+print(int_var, " is of type ", type(int_var))
+
+# Float
+float_var = 10.25  # All numbers with decimal points are float
+print(float_var, " is of type ", type(float_var))
+
+# Complex
+complex_var = 10j  # All numbers with imaginary part are complex, imaginary part is represented by j.
+print(complex_var, " is of type ", type(complex_var))
+
+# Boolean
+true_var = True  # The variables that contain only True/False values are called boolean variables
+false_var = False
+print(true_var, " is of type ", type(true_var))
+print(false_var, " is of type ", type(false_var))
