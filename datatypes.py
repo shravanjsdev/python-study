986c64e2c6787db5712fa9e5f1e832d9858d3d47
shravanjsdev <sshravan.js@gmail.com>
2020-06-07 22:54:40 +0530

    Datatypes

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+# Data types
+print("There are 6 data types supported in python: ")
+print("1. Numbers")
+print("2. String")
+print("3. List")
+print("4. Dictionary")
+print("5. Tuple")
+print("6. Set")
+
+# Another data type that is supported in python is "Range"
+# It is used to iterate through the values in the variables.
+
+
+
+
+
+
+
+
+
