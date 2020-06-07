9617c6ff40843e1fae219bd628b235c6885383a2
shravanjsdev <sshravan.js@gmail.com>
2020-06-07 22:28:15 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Variables are used to store values in the memory, they are the memory
+# blocks that stores the values temporarily/permanently
+
+"""
+Unlike programming languages like C and C++, variable declaration is not allowed in python
+It should be assigned values at the time of its usage, it shouldn't be declared empty
+Variables are generic, no need for mentioned variables data type
+# At the time of definition variables take the data type of value automatically
+# They are case sensitive
+"""
+
+name = 'Shravan'
+emp_id = 7330
+org = 'Zoho'
+
+print(name)
+print(emp_id)
+print(org)
