bf4d25deb7e80d6ff12bf45ab1a490004547e92e
shravanjsdev <sshravan.js@gmail.com>
2020-06-10 01:14:17 +0530

    Range datatype

---
+++ range-datatype.py
@@ -0,0 +1,10 @@
+# Range datatype
+# Returns a collection of numbers starting from 0 to the range number given in argument
+
+print(range(10))
+
+list_var = list(range(10))
+print(list_var)
+
+tuple_var = tuple(range(10))
+print(tuple_var)
