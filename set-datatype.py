c5b2174ef7679b9b023c6da834bc3310585b3af0
shravanjsdev <sshravan.js@gmail.com>
2020-06-10 01:08:53 +0530

    Set datatype

---
+++ set-datatype.py
@@ -0,0 +1,8 @@
+# Set datatype
+
+# Unordered, heterogeneous collection of data
+# No duplicate entries are present, it will overwrite
+# Cannot be accessed using its index number
+
+set_var = {1, 2, 3, 'One', 'Two', 'Three', 1, 2, 3, 'One', 'Two', 'Three'}
+print(set_var)
