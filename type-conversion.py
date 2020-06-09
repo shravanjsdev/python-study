55add4bf6bd4dcca06b50532b0dc1fc144f25252
shravanjsdev <sshravan.js@gmail.com>
2020-06-10 01:34:03 +0530

    Type conversion

---
+++ type-conversion.py
@@ -0,0 +1,23 @@
+# Type conversion
+
+# Available datatypes methods
+# 1. int() - This method converts any possible datatype to integer datatype.
+# 2. float() - This method converts any possible datatype to float datatype.
+# 3. str() - This method converts any possible datatype to string datatype.
+# 4. list() - This method converts any possible datatype to list datatype.
+# 5. set() - This method converts any possible datatype to set datatype.
+# 6. dict() - This method converts any possible datatype to dictionary datatype.
+# 7. tuple() - This method converts any possible datatype to tuple datatype.
+
+a = 10  # Integer
+b = str(a)  # String
+c = float(a)  # Float
+d = int(c)  # Int
+e = list(range(5))  # Range
+f = tuple(e)
+g = set(f)
+
+print(a, b, c, d, e, f, g)
+
+
+
