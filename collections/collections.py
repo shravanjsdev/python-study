4d0846c3ebcdcfa28ebbd421c85e189bf8d33bdf
shravanjsdev <sshravan.js@gmail.com>
2020-06-10 14:03:33 +0530

    Collections

---
+++ collections/collections.py
@@ -0,0 +1,31 @@
+# Collections in python
+
+# They are the container datatypes, basically there are 4 container datatypes in python
+# They are used for storing a collection of data
+# There are 4 types of container collection datatypes in python:
+# 1. List
+# 2. Tuple
+# 3. Set
+# 4. Dictionary
+
+# Python has another datatype named as collection itself, apart from the other 4 datatypes.
+# The Collection datatype has specialised data structures, which overcomes the drawbacks of those 4 collection datatypes
+# Such as cannot accessible using index number, duplicate entries not allowed, unordered.
+# Those 4 container datatypes are already installed and auto imported in python so you don't need to install/import it
+# But this collection datatype, is installed in python like others but is not imported, we need to import while using it
+
+# Types of collection datatypes:
+# 1. namedtuple
+# 2. chainmap
+# 3. deque
+# 4. counter
+# 5. ordereddict
+# 6. defaultdict
+# 7. userdict
+# 8. userlist
+# 9. userstring
+
+
+
+
+
