fdc818f1fe0f3966d2b3248cd956bf1adfac76e6
shravanjsdev <sshravan.js@gmail.com>
2020-06-21 00:44:00 +0530

    Operators

---
+++ operators/operators.py
@@ -0,0 +1,12 @@
+# Operators
+
+# The operators which I know and used in other languages
+# 1. Arithmetic operator
+# 2. Logical operator
+# 3. Assignment operator
+# 4. Comparison operator
+# 5. Bitwise operator
+#
+# The operators which are new to me that I haven't heard about or used it before
+# 6. Membership operator
+# 7. Identity operator
