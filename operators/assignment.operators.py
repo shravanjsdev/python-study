392867d4cc1ccb86b61e3483ec642ac080c0310f
shravanjsdev <sshravan.js@gmail.com>
2020-06-21 01:13:08 +0530

    Assignment operators

---
+++ operators/assignment.operators.py
@@ -0,0 +1,57 @@
+# Assignment operators
+
+# 1. Assign
+x = 10
+print("value of x is ", str(x))
+
+# 2. Add and
+x = 10
+x += 10  # x = x + 10
+print("value of x+=10 is", x)
+
+# 3. Subtract and assign
+x = 10
+x -= 10  # x = x - 10
+print("value of x-=10 is", x)
+
+# 4. Multiply and assign
+x = 10
+x *= 10  # x = x * 10
+print("value of x*=10 is ", x)
+
+# 5. Division and assign
+x = 10
+x /= 10  # x = x / 10
+print("value of x/=10 is ", x)
+
+# 6. Modulus and assign
+x = 10
+x %= 10  # x = x % 10
+print("value of x%=10 is", x)
+
+# 7. Exponent and assign
+x = 10
+x **= 10  # x = x ** 10
+print("value of x**=10 is", x)
+
+# 8. Float division and assign
+x = 10
+x //= 10  # x = x // 10
+print("value of x//=10 is", x)
+
+# Should know the exact usage and name of the below 3 operators
+
+# 9. OR and assign
+x = 25
+x |= 15  # x = x | 15
+print("value of x|=10 is", x)
+
+# 10. Cap and assign
+x = 25
+x ^= 15  # x = x ^ 15
+print("value of x^=10 is", x)
+
+# 11. AND and assign
+x = 25
+x &= 15  # x = x & 15
+print("value of x&=10 is", x)
