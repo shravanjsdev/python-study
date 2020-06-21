639a2a079d614f975e86b4f039f1435f95b08a58
shravanjsdev <sshravan.js@gmail.com>
2020-06-21 21:27:44 +0530

    Comparison operator

---
+++ operators/comparison.operators.py
@@ -0,0 +1,28 @@
+# Comparison operators
+
+# 1. Equal operator
+x = 10
+y = 20
+z = 10
+print("x(" + str(x) + ") == y(" + str(y) + ") is", x == y)
+print("x(" + str(x) + ") == z(" + str(z) + ") is", x == z)
+
+# 2. Not equal to
+print("x(" + str(x) + ") != y(" + str(y) + ") is", x != y)
+print("x(" + str(x) + ") != z(" + str(z) + ") is", x != z)
+
+# 3. Greater than
+print("x(" + str(x) + ") > y(" + str(y) + ") is", x > y)
+print("x(" + str(x) + ") > z(" + str(z) + ") is", x > z)
+
+# 4. Lesser than
+print("x(" + str(x) + ") < y(" + str(y) + ") is", x < y)
+print("x(" + str(x) + ") < z(" + str(z) + ") is", x < z)
+
+# 5. Greater than or equal to
+print("x(" + str(x) + ") >= y(" + str(y) + ") is", x >= y)
+print("x(" + str(x) + ") >= z(" + str(z) + ") is", x >= z)
+
+# 6. Lesser than or equal to
+print("x(" + str(x) + ") <= y(" + str(y) + ") is", x <= y)
+print("x(" + str(x) + ") <= z(" + str(z) + ") is", x <= z)
