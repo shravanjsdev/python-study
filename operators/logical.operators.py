160c73e067ec254e652a8bd1a3b8eb081fe7e130
shravanjsdev <sshravan.js@gmail.com>
2020-06-21 21:43:15 +0530

    Logical operators

---
+++ operators/logical.operators.py
@@ -0,0 +1,17 @@
+# Logical operators
+
+x = 10
+y = 20
+z = 30
+# 1. AND operator
+res = x == 15 or y == 20
+print("res is", res)
+
+# 2. OR operator
+res = x == 15 and y == 20
+print("res is", res)
+
+# 3. NOT operator
+res = not True
+print("res is ", res)
+
